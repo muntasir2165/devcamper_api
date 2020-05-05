7517cbbe97a87d55e08b1a5a91b959355a0c5304
Muntasir Biojid <muntasir2165@hotmail.com>
2020-05-05 00:10:19 -0700

    video-69 rate limiting, hpp & cors

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -50,6 +53,19 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Enable CORS
+app.use(cors());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100,
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
 
 // Set stati folder
 app.use(express.static(path.join(__dirname, 'public')));
